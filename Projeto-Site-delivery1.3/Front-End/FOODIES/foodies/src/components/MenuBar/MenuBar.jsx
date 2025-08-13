c1430483b96bb9f7b4a5bbb07537f2eeaf9309b1
Henrique-Nunes7 <henrique.osasco02@gmail.com>
2025-08-13 05:53:41 -0300

    ssfsf
    sdfsf

---
+++ Projeto-Site-delivery1.3/Front-End/FOODIES/foodies/src/components/MenuBar/MenuBar.jsx
@@ -0,0 +1,72 @@
+import React, { useContext, useState } from "react";
+import "./Menubar.css";
+import { assets } from "../../assets/assets";
+import { Link, useNavigate } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
+StoreContext;
+
+const MenuBar = () => {
+const [active, setActive] = useState('home');
+
+  const {quantities} = useContext(StoreContext);
+
+  const uniqueItemsInCart = Object.values(quantities).filter(qty => qty > 0).length;
+ const navigate = useNavigate();
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container">
+       <Link to='/'> <img src={assets.logo} alt="" className="mx-4" height={48} width={48} /></Link> 
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className={active === "home" ? " nav-link fw-bold active": "nav-link"} to="/" onClick={() => setActive('home')}>
+                Início
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className={active === 'explore' ?"nav-link fw-bold active": "nav-link"} to="/explore" onClick={() => setActive('explore')}>
+                Explorar
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className={active === 'contact-us' ?"nav-link fw-bold active": "nav-link"} to="/contact" onClick={() => setActive('contact-us')}>
+                Fale conosco
+              </Link>
+            </li>
+          </ul>
+          <div className="d-flex align-items-center gap-2">
+            <Link to={`/cart`}>
+            <div className="position-relative m-3">
+              <img
+                src={assets.cart}
+                alt="Carrinho"
+                height={32}
+                width={32}
+                className="position-relative"
+              />
+              <span className="cart-badge position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning">
+                {uniqueItemsInCart}
+              </span> 
+            </div>
+            </Link >
+            <button className="btn btn-outline-primary" onClick={() => navigate('/login')}>Login</button>
+            <button className="btn btn-outline-success" onClick={() => navigate('/register')}>Registrar-se</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default MenuBar;
